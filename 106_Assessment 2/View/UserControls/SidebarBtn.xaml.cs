cb67a5e97b5db45ea18b6b4cb6ace6476658abe5
sachinstha9 <sachinshrestha7055@gmail.com>
2025-11-04 12:12:16 +1300

    usercontrol element created for side bar button

---
+++ 106_Assessment 2/View/UserControls/SidebarBtn.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace _106_Assessment_2.View.UserControls
+{
+    /// <summary>
+    /// Interaction logic for SidebarBtn.xaml
+    /// </summary>
+    public partial class SidebarBtn : UserControl
+    {
+        public SidebarBtn()
+        {
+            InitializeComponent();
+        }
+
+        public static readonly DependencyProperty PageNameProperty =
+            DependencyProperty.Register(
+                "PageName",
+                typeof(string),
+                typeof(SidebarBtn),
+                new PropertyMetadata(string.Empty));
+
+        public string PageName
+        {
+            get => (string)GetValue(PageNameProperty);
+            set => SetValue(PageNameProperty, value);
+        }
+
+        public static readonly DependencyProperty PageIconProperty =
+            DependencyProperty.Register(
+                "PageIcon",
+                typeof(string),
+                typeof(SidebarBtn),
+                new PropertyMetadata(string.Empty));
+
+        public string PageIcon
+        {
+            get => (string)GetValue(PageIconProperty);
+            set => SetValue(PageIconProperty, value);
+        }
+    }
+}
