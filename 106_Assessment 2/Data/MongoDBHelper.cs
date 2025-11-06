1a916e56c7c809d181924b916b418fdffeb7be00
270579408 <270579408@yoobeestudent.ac.nz>
2025-11-07 11:23:03 +1300

    Refactor MongoDB connection setup in MongoDBHelper
    Updated MongoDB connection string and improved connection handling.

--- 106_Assessment 2/Data/MongoDBHelper.cs
+++ 106_Assessment 2/Data/MongoDBHelper.cs
@@ -1,4 +1,4 @@
-﻿using MongoDB.Driver;
+using MongoDB.Driver;
 using MongoDB.Bson;
 
 namespace _106_Assessment_2.Data
@@ -9,19 +9,22 @@ namespace _106_Assessment_2.Data
 
         public MongoDBHelper()
         {
-         const string connectionUri = "mongodb+srv://270579408:<db_password>@cluster0.xicz7cs.mongodb.net/?appName=Cluster0";
-var settings = MongoClientSettings.FromConnectionString(connectionUri);
-// Set the ServerApi field of the settings object to set the version of the Stable API on the client
-settings.ServerApi = new ServerApi(ServerApiVersion.V1);
-// Create a new client and connect to the server
-var client = new MongoClient(settings);
-// Send a ping to confirm a successful connection
-try {
-  var result = client.GetDatabase("admin").RunCommand<BsonDocument>(new BsonDocument("ping", 1));
-  Console.WriteLine("Pinged your deployment. You successfully connected to MongoDB!");
-} catch (Exception ex) {
-  Console.WriteLine(ex);
-}
+            const string connectionUri = "mongodb+srv://270579408:S692718m@cluster0.xicz7cs.mongodb.net/?appName=Cluster0";
+            var settings = MongoClientSettings.FromConnectionString(connectionUri);
+            // Set the ServerApi field of the settings object to set the version of the Stable API on the client
+            settings.ServerApi = new ServerApi(ServerApiVersion.V1);
+            // Create a new client and connect to the server
+            var client = new MongoClient(settings);
+            // Send a ping to confirm a successful connection
+            try
+            {
+                var result = client.GetDatabase("admin").RunCommand<BsonDocument>(new BsonDocument("ping", 1));
+                Console.WriteLine("Pinged your deployment. You successfully connected to MongoDB!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
     }
-}
 }
