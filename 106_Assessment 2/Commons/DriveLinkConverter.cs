a3d418109a7b8eb9f0098793e508026cfbd7560e
sachinstha9 <sachinshrestha7055@gmail.com>
2025-11-06 20:59:18 +1300

    google drive link converter created

---
+++ 106_Assessment 2/Commons/DriveLinkConverter.cs
@@ -0,0 +1,20 @@
+﻿namespace _106_Assessment_2.Commons
+{
+    public static class DriveLinkConverter
+    {
+        public static string converter(string link)
+        {
+            if (string.IsNullOrWhiteSpace(link))
+                return string.Empty;
+
+            string[] splitedLink = link.Split("/");
+
+            if (splitedLink.Length < 6)
+                return link;
+
+            string newLink = "https://drive.google.com/uc?export=view&id=" + splitedLink[splitedLink.Length - 2];
+
+            return newLink;
+        }
+    }
+}
