d40545270e5ba8d22cad18c012e5382798baf545
sachinstha9 <sachinshrestha7055@gmail.com>
2025-11-07 00:51:08 +1300

    Some updates for upcoming events scroll

--- 106_Assessment 2/View/Pages/Home.xaml.cs
+++ 106_Assessment 2/View/Pages/Home.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using _106_Assessment_2.Models;
 using _106_Assessment_2.ViewModels;
 using MongoDB.Driver;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Threading;
@@ -11,6 +13,10 @@ namespace _106_Assessment_2.View.Pages
     public partial class Home : Page
     {
         private readonly EventViewModel _eventViewModel;
+
+        private Point _startPoint;
+        private double _startOffset;
+        private bool _isDragging = false;
 
         private DispatcherTimer _timer;
         private int _currentIndex = 0;
@@ -86,6 +92,30 @@ namespace _106_Assessment_2.View.Pages
             _currentIndex = (_currentIndex + 1) % FeaturedEvents.Count;
             ShowBanner(_currentIndex);
             ResetBannerTimer();
+        }
+
+        private void ScrollViewer_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            _isDragging = true;
+            _startPoint = e.GetPosition(EventScrollViewer);
+            _startOffset = EventScrollViewer.HorizontalOffset;
+            EventScrollViewer.CaptureMouse();
+        }
+
+        private void ScrollViewer_PreviewMouseMove(object sender, MouseEventArgs e)
+        {
+            if (!_isDragging) return;
+
+            Point currentPoint = e.GetPosition(EventScrollViewer);
+            double delta = _startPoint.X - currentPoint.X;
+
+            EventScrollViewer.ScrollToHorizontalOffset(_startOffset + delta);
+        }
+
+        private void ScrollViewer_PreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            _isDragging = false;
+            EventScrollViewer.ReleaseMouseCapture();
         }
     }
 }
