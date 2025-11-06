d481e7addb88b3558db464730ab47dd9dfdb97fa
270579408 <270579408@yoobeestudent.ac.nz>
2025-11-07 09:40:02 +1300

    Refactor MongoDB connection and add ping check
    Updated MongoDB connection setup to use MongoClientSettings and added a ping command to verify connection.

--- 106_Assessment 2/Data/MongoDBHelper.cs
+++ 106_Assessment 2/Data/MongoDBHelper.cs
@@ -1,4 +1,5 @@
 ﻿using MongoDB.Driver;
+using MongoDB.Bson;
 
 namespace _106_Assessment_2.Data
 {
@@ -8,15 +9,19 @@ namespace _106_Assessment_2.Data
 
         public MongoDBHelper()
         {
-            var connectionString = "mongodb+srv://270579408:S692718m@cluster0.xicz7cs.mongodb.net/?appName=Cluster0";
-            var client = new MongoClient(connectionString);
-
-            _database = client.GetDatabase("OnewheroBayPark");
-        }
-
-        public IMongoCollection<T> GetCollection<T>(string collectionName)
-        {
-            return _database.GetCollection<T>(collectionName);
-        }
+         const string connectionUri = "mongodb+srv://270579408:<db_password>@cluster0.xicz7cs.mongodb.net/?appName=Cluster0";
+var settings = MongoClientSettings.FromConnectionString(connectionUri);
+// Set the ServerApi field of the settings object to set the version of the Stable API on the client
+settings.ServerApi = new ServerApi(ServerApiVersion.V1);
+// Create a new client and connect to the server
+var client = new MongoClient(settings);
+// Send a ping to confirm a successful connection
+try {
+  var result = client.GetDatabase("admin").RunCommand<BsonDocument>(new BsonDocument("ping", 1));
+  Console.WriteLine("Pinged your deployment. You successfully connected to MongoDB!");
+} catch (Exception ex) {
+  Console.WriteLine(ex);
+}
     }
+}
 }
