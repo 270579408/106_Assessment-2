c8e96a92253ff71793fe4caeaa3d854a876e259c
sachinstha9 <sachinshrestha7055@gmail.com>
2025-11-13 15:45:10 +1300

    Some parts of help / contact page finished

--- 106_Assessment 2/MainWindow.xaml.cs
+++ 106_Assessment 2/MainWindow.xaml.cs
@@ -10,7 +10,7 @@ namespace _106_Assessment_2
         public MainWindow()
         {
             InitializeComponent();
-            MainFrame.Navigate(new View.Pages.Home());
+            MainFrame.Navigate(new View.Pages.Help());
         }
 
         private void MenuToggleButton_Click(object sender, RoutedEventArgs e)
@@ -28,7 +28,7 @@ namespace _106_Assessment_2
         private void Things_Click(object sender, RoutedEventArgs e) => MainFrame.Navigate(new View.Pages.Activities());
         private void Park_Click(object sender, RoutedEventArgs e) => MainFrame.Navigate(new View.Pages.ParkInfo());
         private void Community_Click(object sender, RoutedEventArgs e) => MainFrame.Navigate(new View.Pages.Community());
-        private void Help_Click(object sender, RoutedEventArgs e) => MessageBox.Show("Contact us at info@onewhero.nz");
+        private void Help_Click(object sender, RoutedEventArgs e) => MainFrame.Navigate(new View.Pages.Help());
         private void Settings_Click(object sender, RoutedEventArgs e) => MessageBox.Show("Settings Page coming soon!");
         private void SignIn_Click(object sender, RoutedEventArgs e) => MessageBox.Show("a");
     }
--- 106_Assessment 2/View/Pages/Help.xaml.cs
+++ 106_Assessment 2/View/Pages/Help.xaml.cs
@@ -1,15 +1,21 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
 
 namespace _106_Assessment_2.View.Pages
 {
-    /// <summary>
-    /// Interaction logic for Help.xaml
-    /// </summary>
     public partial class Help : Page
     {
         public Help()
         {
             InitializeComponent();
+            DataContext = this;
+        }
+
+        private void InquiryInput_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Placeholder_InquiryInput.Visibility = string.IsNullOrWhiteSpace(InquiryInput.Text)
+                ? Visibility.Visible
+                : Visibility.Hidden;
         }
     }
 }
