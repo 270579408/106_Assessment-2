629524e988a7dbaa7eb5d112cb5a2fe3ef64ad44
sachinstha9 <sachinshrestha7055@gmail.com>
2025-11-14 09:05:09 +1300

    Register View Model created

---
+++ 106_Assessment 2/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,27 @@
+﻿using MongoDB.Driver;
+using _106_Assessment_2.Data;
+using _106_Assessment_2.Models;
+
+namespace _106_Assessment_2.ViewModels
+{
+    public class RegisterViewModel
+    {
+        private readonly IMongoCollection<User> _user;
+
+        public RegisterViewModel()
+        {
+            var dbHelper = new MongoDBHelper();
+            _user = dbHelper.GetCollection<User>("User");
+        }
+
+        public void AddUser(User e)
+        {
+            _user.InsertOne(e);
+        }
+
+        public List<User> GetAllUser()
+        {
+            return _user.Find(_ => true).ToList();
+        }
+    }
+}
